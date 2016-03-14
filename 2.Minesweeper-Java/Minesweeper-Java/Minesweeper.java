29b9332b77efd1d0ccd3eab5f1a416eb6b25ce9b
Axel <axelbaxel@gmail.com>
2016-03-14 15:06:50 +0100

    Some fixes + new SMproj file

--- 2.Minesweeper-Java/Minesweeper-Java/Minesweeper.java
+++ 2.Minesweeper-Java/Minesweeper-Java/Minesweeper.java
@@ -51,19 +51,7 @@ public class Minesweeper {
 				}
 				continue;
 			}
-//			if(input.equals("exit")){
-//				rank.recordName(result);
-//				return false;
-//			}
-//			if(field.legalMoveString(input)){
-//				result++;
-//				if(result==35){
-//					System.out.println("Congratulations you WON the game!");
-//					rank.recordName(result);
-//					return true;
-//				}
-//				continue;
-//			}
+
 			else if (field.getBoom()) {
 				System.out.println("\nBooooooooooooooooooooooooooooom!You stepped on a mine!You survived " + result + " turns");
 				rank.recordName(result);
@@ -79,10 +67,10 @@ public class Minesweeper {
 	private static void mainMessage(){
 		System.out.println("Welcome to Minesweeper!");
 		System.out.println("To play just input some coordinates and try not to step ont mine :)");
-		System.out.println("Usefull commands:");
-		System.out.println("restart- Starts a new game.");
-		System.out.println("exit- Quits the game.");
-		System.out.println("top- Reveals the top scoreboard.");
+		System.out.println("Useful commands:");
+		System.out.println("restart - Starts a new game.");
+		System.out.println("exit - Quits the game.");
+		System.out.println("top - Reveals the top scoreboard.");
 		System.out.println("Have Fun !");
 	}
 }
